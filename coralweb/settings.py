72905463587e6e2d33513dc331b5b3a7e72f660b
Wesin Alves <wesinec@gmail.com>
2025-05-27 22:08:49 -0300

    update media folder

--- coralweb/settings.py
+++ coralweb/settings.py
@@ -122,6 +122,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
