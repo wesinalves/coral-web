b4dfe2306f05561ca66d47aa9b316b1bdf0fb2c5
Wesin Alves <wesinec@gmail.com>
2025-05-30 16:44:51 -0300

    add register view

--- webapp/views.py
+++ webapp/views.py
@@ -5,8 +5,10 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import login, logout, authenticate
-from .models import Music, Event, Notice
+from .models import Music, Event, Notice, Member, User
 from django.core.paginator import Paginator
+from .forms import RegisterForm, ProfileForm
+from datetime import datetime
 
 # Create your views here.
 
@@ -15,27 +17,6 @@ NUMROWS=10
 @login_required
 def index(request):
     return render(request, 'index.html')
-
-
-def register(request):
-    # if request.method != 'POST':
-    #     form = MemberForm()
-    # else:
-    #     form = MemberForm(request.POST)
-    #     if form.is_valid():
-    #         user = form.save(commit=False)
-    #         user.created_at = datetime.today().strftime('%Y-%m-%d')
-    #         user.password = make_password(form.cleaned_data['password'])
-    #         user.save()
-    #         return HttpResponseRedirect(reverse('index'))
-    #     else:
-    #         print(form.cleaned_data.get("photo"))
-    
-    # context = {'form': form}
-
-    return render(request, 'register.html')
-
-
 
 @login_required
 def logout_view(request):
@@ -92,3 +73,63 @@ def calendar(request):
 
     context = {'breadcrumbs': breadcrumbs, 'events': events}
     return render(request, 'calendar.html', context)
+
+@login_required
+def profile(request):
+    """Show user profile."""
+    breadcrumbs = [
+        {'title': 'Home', 'url': reverse('index')},
+        {'title': 'Perfil', 'url': None},
+    ]
+
+    try:
+        member = Member.objects.get(user_id=request.user.id)
+    except Member.DoesNotExist:
+        member = None
+
+    
+    context = {
+        'breadcrumbs': breadcrumbs,
+        'member': member,
+    }
+    return render(request, 'profile.html', context)
+
+@login_required
+def edit_profile(request):
+    breadcrumbs = [
+        {'title': 'Home', 'url': reverse('index')},
+        {'title': 'Perfil', 'url': reverse('profile')},
+        {'title': 'Editar Perfil', 'url': None},
+    ]
+    try:
+        logged_user = Member.objects.get(user_id=request.user.id)
+    except Member.DoesNotExist:
+        logged_user = get_object_or_404(User, id=request.user.id)
+
+    if request.method == 'POST':
+        form = ProfileForm(instance=logged_user)
+    else:
+        form = ProfileForm(request.POST, instance=logged_user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+        
+
+def register(request):
+    if request.method != 'POST':
+        form = RegisterForm()
+    else:
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.created_at = datetime.today().strftime('%Y-%m-%d')
+            user.password = make_password(form.cleaned_data['password'])
+            user.save()
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.cleaned_data.get("photo"))
+    
+    context = {'form': form}
+
+    return render(request, 'register.html', context)
+    
