9f1629375db5db3a6759afcd0526a06061061e12
Wesin Alves <wesinec@gmail.com>
2025-06-05 15:42:59 -0300

    edit settings for static files

--- coralweb/settings.py
+++ coralweb/settings.py
@@ -130,6 +130,10 @@ MEDIA_URL = '/media/'
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
